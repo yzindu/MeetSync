4f2f4dacef43bfb6c4f7c47338cb3d8f611675d0
Yasindu Janapriya <yasindurivipiyum@gmail.com>
2026-07-08 12:25:38 +0530

    feat: create axios.js to connect with the backend

---
+++ frontend/src/api/axios.js
@@ -0,0 +1,21 @@
+import axios from 'axios';
+
+const API = axios.create({
+    baseURL: 'http://localhost:5000/api', // Point to Node.js server
+});
+
+API.interceptors.request.use(
+    (config) => {
+        const token = localStorage.getItem('token');
+
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+);
+
+export default API;
